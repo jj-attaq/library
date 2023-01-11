cfda2e189e0134fc2c574f6336987954469957b5
jj.attaq <jj.attaq@tutanota.com>
2023-01-11 15:04:00 +0100

    addbook btn functional, books display on form submission

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 "use strict";
 window.onload=function() {
   addModal()
+  addBook()
 }
 const myLibrary = [];
 
@@ -8,12 +9,6 @@ function Book(title, author, pages, read) {
   this.title = title
   this.author = author
   this.pages = pages
-  //this.read = true ? 'has been read' : 'has not been read yet'
-  /*
-  this.title.classList.add('book-title');
-  this.author.classList.add('book-author');
-  this.pages.classList.add('book-pages');
-  */
 }
 
 Book.prototype.read = function() {
@@ -30,22 +25,33 @@ myLibrary.push(dune, hobbit);
 function displayLibrary(library) {
   return library.map(book => displayBook(book));
 }
-/*
 function addBook() {
-  const addBook = document.querySelector('#addBook');
-  addBook.addEventListener('click', () => {
-    alert('test')
-  });
+  const form = document.querySelector('#form');
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const title = document.getElementById('title');
+    const author = document.getElementById('author');
+    const pages = document.getElementById('pages');
+    const readStatus = document.getElementById('readStatus');
+    const newBook = new Book(`${title.value}`, `${author.value}`, `${pages.value}`, `${readStatus.value}`)
+    const refreshForm = () => {
+      title.value = '';
+      author.value = '';
+      pages.value = '';
+      readStatus.value = '';
+    }
+    myLibrary.push(newBook);
+    displayBook(newBook);
+    refreshForm();
+  })
 }
-*/
 function addModal() {
   const modal = document.querySelector('#modal');
-  const openModal = document.querySelector('#addBook');
-  const closeModal = document.querySelector('#closeModal');
+  const openModal = document.querySelector('#openModal');
   openModal.addEventListener('click', () => {
     modal.showModal();
   })
-
+  const closeModal = document.querySelector('#addBook');
   closeModal.addEventListener('click', () => {
     modal.close();
   })
@@ -56,15 +62,23 @@ function displayBook(book) {
 
   const title = document.createElement('div')
   title.classList.add('book-title')
+
   const author = document.createElement('div')
   author.classList.add('book-author')
+
   const pages = document.createElement('div')
   pages.classList.add('book-pages')
+
   const read = document.createElement('div')
   read.classList.add('book-readStatus')
 
-  booksContainer.append(title, author, pages, read);
+  const remove = document.createElement('div')
+  remove.classList.add('book-removal')
+  const removeButton = document.createElement('button')
+  removeButton.classList.add('bookRemoval')
+  removeButton.textContent = 'X';
 
-  return title.append(book.title), author.append(book.author), pages.append(book.pages), read.append(book.read());
-  //return card.append(book.info());
+  booksContainer.append(title, author, pages, read, remove);
+
+  return title.append(book.title), author.append(book.author), pages.append(book.pages), read.append(book.read()), remove.append(removeButton);
 }
