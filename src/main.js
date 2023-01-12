3ae8f67949b3e2959f746a5ffbb4af852044d2fa
jj.attaq <jj.attaq@tutanota.com>
2023-01-12 18:10:13 +0100

    readStatus updateable on DOM, frankenstein code needs cleaning

--- src/main.js
+++ src/main.js
@@ -16,6 +16,9 @@ Book.prototype.read = function(readStatus) {
   readStatus = this.checkedOrNot;
   return readStatus === true ? 'has been read' : 'has not been read yet';
 }
+Book.prototype.reference = function() {
+  return myLibrary.indexOf(this);
+}
 Book.prototype.info = function() {
   return `"${this.title}" by ${this.author}, is ${this.pages} pages long, and ${this.read()}.`
 }
@@ -67,7 +70,7 @@ function addModal() {
   })
 }
 function removeBook() {
-  const bookRef = document.querySelector('#remove-book-' + book.listPlacement)
+  const bookRef = document.querySelector('#remove-book')
   console.log(bookRef);
 
   bookRef.addEventListener('click', () => {
@@ -75,29 +78,46 @@ function removeBook() {
     return title.remove(), author.remove(), pages.remove(), read.remove(), remove.remove();
   })
 }
+removeBook();
 function displayBook(book) {
   const booksContainer = document.querySelector('.books-container');
+  const para = document.createElement('p');
 
-  const title = document.createElement('div')
-  title.classList.add('book-title')
+  const title = document.createElement('div');
+  title.classList.add('book-title');
 
-  const author = document.createElement('div')
-  author.classList.add('book-author')
+  const author = document.createElement('div');
+  author.classList.add('book-author');
 
-  const pages = document.createElement('div')
-  pages.classList.add('book-pages')
+  const pages = document.createElement('div');
+  pages.classList.add('book-pages');
 
-  const read = document.createElement('div')
-  read.classList.add('book-readStatus')
+  const read = document.createElement('div');
+  read.classList.add('book-readStatus');
 
-  const readBox = document.createElement('input')
+  const readBox = document.createElement('input');
+  let checkedOrNot = book.checkedOrNot;
+  readBox.classList.add('readBox');
+  readBox.setAttribute('id', `readBox-book-${book.listPlacement}`);
   readBox.type = 'checkbox';
   
-  const remove = document.createElement('div')
-  remove.classList.add('book-removal')
-  const removeButton = document.createElement('button')
-  removeButton.classList.add('bookRemoval')
-  removeButton.setAttribute('id', `remove-book-${book.listPlacement}`)
+  readBox.addEventListener('change', () => {
+    if(readBox.checked) {
+      book.checkedOrNot = true;
+      read.textContent = book.read();
+      read.append(readBox);
+    } else {
+      book.checkedOrNot = false;
+      read.textContent = book.read();
+      read.append(readBox);
+    }
+  })
+  
+  const remove = document.createElement('div');
+  remove.classList.add('book-removal');
+  const removeButton = document.createElement('button');
+  removeButton.classList.add('bookRemoval');
+  removeButton.setAttribute('id', `remove-book`);
   removeButton.textContent = 'X';
 
   booksContainer.append(title, author, pages, read, remove);
